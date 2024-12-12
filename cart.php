4d1f15298271aef2b7eab592ab983236b0199126
Abdelrahman <abdelrahman24689@gmail.com>
2024-12-12 19:19:53 +0200

    fix: fixed cart.php

--- cart.php
+++ cart.php
@@ -1,27 +1,11 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https//pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEc3Yw5cVb3zcuHt0A93w35dYTsvhLPVnYs9eStHfGJvOvkxVfELGroGkvsg+p" crossorigin="anonymous" >
-
-    <link rel="stylesheet" href="assets/css/style.css">
-    
-</head>
-<body>
-
-    <!--Cart-->
+<?php 
+    $connect =new PDO("mysql:host=localhost;dbname=if0_37067887_crs",'root','');
+    $sql = $connect->prepare("SELECT * FROM car WHERE Plate_ID = :id");
+    $sql-> bindParam(':id',$_GET['id']);
+    $sql->execute();
+    $car = $sql->fetch(PDO::FETCH_ASSOC);
+    include('templates/head.html');
+?>
     <section class="cart container my-5 py-5" >
         <div class="container mt-5 " >
             <h2 class="font-weight-bolde">Your Cart</h2>
@@ -36,33 +20,23 @@
             <tr>
                 <td>
                     <div class="car-info" >
-                        <img src="assets/imgs/BMW.jpeg">
+                        <img src="img/f.jpg">
                         <div>
-                        <h1><?php echo $_GET['id']; ?></h1>
-                            <small><span></span></small>
-                            <br>
-                            <form method="POST" action="cart.php" >
-                                <input type="hidden" name="Plate_ID" value="<?php echo $row['Plate_ID'];  ?>" />
-                                <input type="submit" name="remove_car" class="remove-btn" value="remove" >
-                            </form>
-                            
+                            <h1>Model: <?php echo $car['Model']; ?></h1>
+                            <h2>Type: <?php echo $car['type']; ?></h2>
+                            <h2>Car Name: <?php echo $car['name']; ?></h2>
+                            <h4>Status: <?php echo $car['Status']; ?></h2>
                         </div>
                     </div>
                 </td>
-                
                 <td>
-                
                 </td>
-
                 <td>
                     <span>$</span>
-                    <span class="car-price" >155</span>
+                    <span class="car-price" ><?php echo $car['price']; ?></span>
                 </td>
             </tr>
         </table>
-
-
-
         <div class="cart-total">
             <table>
                 <tr>
@@ -71,21 +45,13 @@
                 </tr>
             </table>
         </div>
-
-
         <div class="checkout-container">
             <form method="POST" action="checkout.php" >
-                <input type="submit" class="btn checkout-btn" value="Checkout" name="checkout" >
+                <input type="hidden" name="pltid" value="<?php echo $_GET['id'] ?>">
+                <input type="submit" class="btn primary bg-primary checkout-btn" value="Checkout" name="checkout" >
             </form>
-            
         </div>
-
     </section>
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+   <?php
+    include('templates/tail.html');
+   ?>
--- checkout.php
+++ checkout.php
@@ -1,34 +1,12 @@
-<?php
-
-session_start();
-
-
-if(!empty($_SESSION['cart']))
-
+<?php 
+    $connect =new PDO("mysql:host=localhost;dbname=if0_37067887_crs",'root','');
+    $sql = $connect->prepare("SELECT * FROM car WHERE Plate_ID = :id");
+    $sql-> bindParam(':id',$_GET['id']);
+    $sql->execute();
+    $car = $sql->fetch(PDO::FETCH_ASSOC);
+    include('templates/head.html');
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https//pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEc3Yw5cVb3zcuHt0A93w35dYTsvhLPVnYs9eStHfGJvOvkxVfELGroGkvsg+p" crossorigin="anonymous" >
-
-    <link rel="stylesheet" href="assets/css/style.css">
-    
-</head>
-<body>
     <section class="my-5 py-5" >
         <div class="container text-center mt-3 pt-5" >
             <h2 class="form-weight-bold" >Checkout</h2>
